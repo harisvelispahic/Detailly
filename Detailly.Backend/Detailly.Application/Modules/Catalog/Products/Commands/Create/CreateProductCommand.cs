84a71392d91c18470cc8937dba406f6a8e622a0f
Danis Mušić <danis.music@edu.fit.ba>
2025-11-06 13:45:13 +0100

    Incomplete product-CRUD logic, to be implemented

--- Detailly.Backend/Detailly.Application/Modules/Catalog/Products/Commands/Create/CreateProductCommand.cs
+++ Detailly.Backend/Detailly.Application/Modules/Catalog/Products/Commands/Create/CreateProductCommand.cs
@@ -1,4 +1,6 @@
 ﻿
+using Detailly.Domain.Common.Enums;
+
 namespace Detailly.Application.Modules.Catalog.Products.Commands.Create;
 
 public class CreateProductCommand : IRequest<int>
@@ -7,5 +9,24 @@ public class CreateProductCommand : IRequest<int>
     public required string Description { get; set; }
     public required decimal Price { get; set; }
     public required int CategoryId { get; set; }
+    public CurrencyName? Currency { get; set; } = default(CurrencyName?);
+    public string? Tags { get; set; }
+    public CreateProductCommandInventory Inventory { get; set; }
+    public List<CreateProductCommandImages>? Images { get; set; } = new List<CreateProductCommandImages>();
+}
 
+public class CreateProductCommandInventory
+{
+    public required int QuantityInStock { get; set; }
+    public int? ReorderLevel { get; set; }
+    public int? ReorderQuantity { get; set; }
+}
+
+public class CreateProductCommandImages
+{
+    public required string ImageUrl { get; set; }
+    public required int ProductId { get; set; }
+    public string? AltText { get; set; }
+    public bool? IsThumbnail { get; set; }
+    public int? DisplayOrder { get; set; }
 }
--- Detailly.Backend/Detailly.Application/Modules/Catalog/Products/Commands/Update/UpdateProductCommand.cs
+++ Detailly.Backend/Detailly.Application/Modules/Catalog/Products/Commands/Update/UpdateProductCommand.cs
@@ -4,9 +4,17 @@ namespace Detailly.Application.Modules.Catalog.Products.Commands.Update;
 public sealed class UpdateProductCommand : IRequest<Unit>
 {
     [JsonIgnore]
-    public int Id { get; set; }
+    public required int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal? Price { get; set; }
     public string? Tags { get; set; } // comma-separated tags
+    public UpdateProductCommandInventory Inventory { get; set; }
+}
+
+public class UpdateProductCommandInventory
+{
+    public int? QuantityInStock { get; set; }
+    public int? ReorderLevel { get; set; }
+    public int? ReorderQuantity { get; set; }
 }
